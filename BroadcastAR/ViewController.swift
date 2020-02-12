487b9c29d7308795a91e25d0c040c8b578dec34e
davidj-bbc <david.johnston@bbc.co.uk>
2020-02-12 16:27:08 +0000

    more comments

--- BroadcastAR/ViewController.swift
+++ BroadcastAR/ViewController.swift
@@ -13,21 +13,26 @@ class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
+    // init our variables for tracking current NotificationTrigger and count
     var allBehaviours: [BroadcastTest.NotificationTrigger]!
     var behaviourCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        // load Reality Composer scene named "Scene" from "broadcastTest" Reality Composer project, XCode capitalises it to
+        // BroadcastTest
         let broadcastAnchor = try! BroadcastTest.loadScene()
+        // add to scen
         arView.scene.anchors.append(broadcastAnchor)
-        
+        // stash the list of behaviours for the scene
         allBehaviours = broadcastAnchor.notifications.allNotifications
 
     }
     
+    // override touchesBegan generic method
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
         if (behaviourCount < allBehaviours.count ) {
             allBehaviours[behaviourCount].post()
             behaviourCount += 1
